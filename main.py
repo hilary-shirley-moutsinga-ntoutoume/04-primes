a107b8bb5922b4c6ff4c9b765057bf75df9ab827
Hilary-Shirley-MOUTSINGA-NTOUTOUME <hilaryshirley.moutsingantoutoume@edu.esiee.fr>
2025-09-22 12:48:36 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,20 +1,50 @@
-from math import sqrt
-
-#### Fonction secondaire
+"""
+Ficher main.py
+Ce programme permet de tester si un  nombre est premier ou pas et affiche
+pour des nombres allant jusuq'à 100 ce qui sont premiers ou pas.
+"""
 
 
 def isprime(p):
+    """
+    Cette fonction permettra de vérifier si un nombre est premier ou pas
+    en renoyant un boléen : 
+    True si le nombre est premier.
+    False dans le cas contraire.
+    Args:
+    p: entier positif.
+    Returns:
+    boolen: True si premier, false si non.
 
-    # votre code ici
+    Si le nombre est <=1, par définition le nombre n'est pas premier, le cas écheant
+    False est donc renvoyé.
+    """
 
-    pass
+    premier = True
+
+    if p<=1:
+        premier = False
+    for i in range(2,p):
+        if p%i==0:
+            premier= False
+    return premier
 
 #### Fonction principale
 
 
 def main():
+    """
+    Ici, on a la fonction principal d'appel à la fonction secondaire.
+    On y envoie des paramètres et un boléen indiquant si le nombre est premier
+    ou pas sera renvoyé.
+    Par ailleurs le programme principal test aussi, pour des nombres de jsuqu'à 100
+    lesquels sont premiers et les affiche.
+    
+    """
 
     # vos appels à la fonction secondaire ici
+    print(isprime(7))
+    print(isprime(1))
 
     for n in range(100):
         if isprime(n):
